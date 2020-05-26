64d98d79e1374038315da4fe53013eeac4008234
pravingoswami <goswami4737@gmail.com>
2020-05-26 10:19:44 +0530

    application join with the mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connection_url = process.env.DB_MONGODB_ATLAS_URL || 'mongodb://localhost:27017home-made-food'
 
 const setupDB = () => {
-    mongoose.connect('mongodb://localhost:27017home-made-food', {useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false})
+    // mongoose.connect('mongodb://localhost:27017home-made-food', {useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false})
+
+    mongoose.connect(connection_url, {useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false})
         .then(() => console.log('Connected with the database'))
 
         .catch(err => console.log(err))
