79f11b9f90743e650447045ae57c9ffa1f563b2b
pravingoswami <goswami4737@gmail.com>
2020-06-01 11:54:34 +0530

    changes for hroku

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const setupDB = require("./config/database")
 const router = require("./config/routes")
 require('dotenv').config()
 const path = require('path')
+const publicDir = path.join(__dirname, './public')
 
 const port = process.env.PORT || 3036
 
 setupDB()
 const app = express()
+app.use(express.static(publicDir))
 app.use(express.json())
-// app.get("/", (req,res) => {
-//     res.json("welcome to our page")
-// })
+
 
 app.use('/', router)
 
