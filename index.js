9de754795e8e23f74124f9c09f77dc44487e2e44
pravingoswami <goswami4737@gmail.com>
2020-06-02 09:54:18 +0530

    install cors npm package

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const router = require("./config/routes")
 require('dotenv').config()
 const path = require('path')
 const publicDir = path.join(__dirname, './public')
+const cors = require("cors")
 
 const port = process.env.PORT || 3036
 
 setupDB()
 const app = express()
+app.use(cors())
 app.use(express.static(publicDir))
 app.use(express.json())
 
